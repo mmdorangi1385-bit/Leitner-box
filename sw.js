f10b9d7ea3760b5ae1e1a6f19394dcb888f5a9de
mmdorangi1385-bit <mmd.orangi.1385@gmail.com>
2026-07-20 01:13:30 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'leitner-cache-v8';
+const CACHE_NAME = 'leitner-cache-v9';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-hero.png', './icon-header.png'];
 
 self.addEventListener('install', (event) => {
